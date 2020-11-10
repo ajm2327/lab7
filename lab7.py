7dd2716818d9884134aab0a0ea5af2dc4cd5eb28
Zachariah Derrick <72170300+ZaderRox1111@users.noreply.github.com>
2020-11-10 16:47:49 -0700

    Add files via upload

--- lab7.py
+++ lab7.py
@@ -20,3 +20,30 @@ def has_contact(contacts, first_name, last_name):
 	except:
 		return False
 		
+def get_contact_string(contacts, first_name, last_name):
+	return contacts[f'{first_name}{last_name}']
+	
+def remove_contact(contacts, first_name, last_name):
+	return contacts.pop(f'{first_name}{last_name}')
+	
+def update_contact_first_name(contacts, first_name, last_name, new_value):
+	contacts[f'{first_name}{last_name}']['first_name'] = new_value
+	contacts[f'{new_value}{last_name}'] = contacts.pop(f'{first_name}{last_name}')
+	return contacts
+	
+def update_contact_last_name(contacts, first_name, last_name, new_value):
+	contacts[f'{first_name}{last_name}']['last_name'] = new_value
+	contacts[f'{first_name}{new_value}'] = contacts.pop(f'{first_name}{last_name}')
+	return contacts
+	
+def update_contact_email(contacts, first_name, last_name, new_value):
+	contacts[f'{first_name}{last_name}']['email'] = new_value
+	return contacts
+	
+def update_contact_phone_number(contacts, first_name, last_name, new_value):
+	contacts[f'{first_name}{last_name}']['phone_number'] = new_value
+	return contacts
+	
+def update_contact_birthday(contacts, first_name, last_name, new_value):
+	contacts[f'{first_name}{last_name}']['birthday'] = new_value
+	return contacts
